e022dc1177f05cfada4ae4e664d33b19181dc95f
raiela <raielaquirino@gmail.com>
2022-02-10 15:27:41 -0300

    Movimentation from oponent 1

--- game-challenge/assets/js/script.js
+++ game-challenge/assets/js/script.js
@@ -7,6 +7,8 @@ function start() { // Inicio da função start()
 	$(".background-game").append("<div id='oponent2' ></div>");
 	$(".background-game").append("<div id='friend' class='anima3'></div>");
 
+    var velocity=5;
+    var posY = parseInt(Math.random() * 334);
     var game = {}
     var KEYWORDS = {
         W: 87,
@@ -31,6 +33,7 @@ function start() { // Inicio da função start()
     function loop(){
         moveBack();
         movePlay();
+        moveOponent1();
     }
 
     function moveBack() {
@@ -45,14 +48,39 @@ function start() { // Inicio da função start()
         if (game.press[KEYWORDS.W]) {
             var top = parseInt($("#player").css("top"));
             $("#player").css("top",top-10);
+
+            if (top<=110) {
+		        $("#player").css("top",top+10);
+            }
         
         }
         
         if (game.press[KEYWORDS.S]) {            
             var top = parseInt($("#player").css("top"));
             $("#player").css("top",top+10);	
+
+            if (top>=530) {
+		        $("#player").css("top",top-10);
+            }
         }
 
+    }
+
+    function moveOponent1(){
+        posX = parseInt($("#oponent1").css("left"));
+        $("#oponent1").css("left",posX-velocity);
+        $("#oponent1").css("top",posY);
+		
+		if (posX<=0) {
+            posY = parseInt(Math.random() * 334);
+            if(posY <= 110){
+                posY = posY+230;
+            }
+            $("#oponent1").css("left",694);
+            $("#oponent1").css("top",posY);			
+		}
+
+        
     }
 
 }
