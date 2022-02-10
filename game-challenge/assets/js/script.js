2a65a0800577a0848e47325e7c2feb922ad937da
raiela <raielaquirino@gmail.com>
2022-02-10 14:58:26 -0300

    Adding action on d and w key.

--- game-challenge/assets/js/script.js
+++ game-challenge/assets/js/script.js
@@ -8,11 +8,29 @@ function start() { // Inicio da função start()
 	$(".background-game").append("<div id='friend' class='anima3'></div>");
 
     var game = {}
+    var KEYWORDS = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    game.press = [];
+    
+    $(document).keydown(function(e){
+        game.press[e.which] = true;
+    });
+    
+    
+    $(document).keyup(function(e){
+        game.press[e.which] = false;
+    });
+    
 
     game.timer = setInterval(loop, 10);
 
     function loop(){
         moveBack();
+        movePlay();
     }
 
     function moveBack() {
@@ -20,6 +38,21 @@ function start() { // Inicio da função start()
         left = parseInt($(".background-game").css("background-position"));
         $(".background-game").css("background-position",left-1);
         
+    }
+
+    function movePlay(){
+
+        if (game.press[KEYWORDS.W]) {
+            var top = parseInt($("#player").css("top"));
+            $("#player").css("top",top-10);
+        
+        }
+        
+        if (game.press[KEYWORDS.S]) {            
+            var top = parseInt($("#player").css("top"));
+            $("#player").css("top",top+10);	
+        }
+
     }
 
 }
