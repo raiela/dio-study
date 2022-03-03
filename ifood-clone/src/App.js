e1db1885e2497ca5f6dcc8e58c9f2947e3f556e5
raiela <raielaquirino@gmail.com>
2022-03-03 15:08:03 -0300

    Add images, configure color and import gesture-handler

--- ifood-clone/src/App.js
+++ ifood-clone/src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IFOOD CLONE!</Text>
+      <Text>Ifood Clone!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- ifood-clone/src/AppEntry.js
+++ ifood-clone/src/AppEntry.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
 import App from './App';
