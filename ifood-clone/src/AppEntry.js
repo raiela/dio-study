4de4b1c4f1e3cdd1367d4b2977c5816952432b45
raiela <raielaquirino@gmail.com>
2022-03-03 15:58:50 -0300

    Finally finished

--- ifood-clone/src/AppEntry.js
+++ ifood-clone/src/AppEntry.js
@@ -1,6 +1,5 @@
-import 'react-native-gesture-handler';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-import App from './App';
+import Routes from './routes';
 
-registerRootComponent(App);
+registerRootComponent(Routes);
